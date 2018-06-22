e47abd8551bd59fb569e5d2c3765aa5f6ffd832b
John Abrahams <jabrah23@gmail.com>
2018-06-22 18:51:31 -0400

    Try to see the actual result

--- app/adapters/fedora-jsonld.js
+++ app/adapters/fedora-jsonld.js
@@ -315,7 +315,10 @@ export default DS.Adapter.extend({
     return this._ajax(url, 'POST', {
       data: JSON.stringify(data),
       headers: {'Content-Type': 'application/json; charset=utf-8'},
-    }).then(result => this._parse_elasticsearch_result(result, info));
+    }).then((result) => {
+      console.log(` ember-fedora-adapter#query\n${result}`);
+      this._parse_elasticsearch_result(result, info);
+    });
   },
 
   // Return the path relative to the adapter root in the Fedora repository
